d79a19a3bb88cf8a9c4756e942132a9be65f2d87
nohsangwoo <nsgr12@naver.com>
2021-05-25 16:46:32 +0900

    #14.6 Create Account part Two

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,33 +2,8 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 import { colors } from '../colors';
-
-const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-  padding: 0px 40px;
-`;
-
-const Logo = styled.Image`
-  max-width: 50%;
-  height: 100px;
-`;
-
-const CreateAccount = styled.TouchableOpacity`
-  background-color: ${colors.blue};
-  padding: 13px 10px;
-  margin-top: 20px;
-  border-radius: 3px;
-  width: 100%;
-  opacity: ${props => (props.disabled ? '0.5' : '1')};
-`;
-const CreateAccountText = styled.Text`
-  color: white;
-  font-weight: 600;
-  text-align: center;
-`;
+import AuthButton from '../components/auth/AuthButton';
+import AuthLayout from '../components/auth/AuthLayout';
 
 const LoginLink = styled.Text`
   color: ${colors.blue};
@@ -40,15 +15,15 @@ export default function Welcome({ navigation }) {
   const goToCreateAccount = () => navigation.navigate('CreateAccount');
   const goToLogIn = () => navigation.navigate('LogIn');
   return (
-    <Container>
-      {/* resizemode: 가로나 세로중 넓은 부분이 100%를 차지할때 까지만 늘리는 것 */}
-      <Logo resizeMode="contain" source={require('../assets/logo.png')} />
-      <CreateAccount disabled={false} onPress={goToCreateAccount}>
-        <CreateAccountText>Create New Account</CreateAccountText>
-      </CreateAccount>
+    <AuthLayout>
+      <AuthButton
+        text="Crate New Account"
+        disabled={false}
+        onPress={goToCreateAccount}
+      />
       <TouchableOpacity onPress={goToLogIn}>
         <LoginLink>Log In</LoginLink>
       </TouchableOpacity>
-    </Container>
+    </AuthLayout>
   );
 }
