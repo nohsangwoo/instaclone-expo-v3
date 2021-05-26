cd8afd318ed645f12ff05cbbba595bddde43400d
nohsangwoo <nsgr12@naver.com>
2021-05-26 21:09:41 +0900

    #14.16 Recap

--- apollo.js
+++ apollo.js
@@ -7,8 +7,8 @@ export const tokenVar = makeVar('');
 // 기존 변수를 기록하기위한 작업
 export const logUserIn = async token => {
   await AsyncStorage.multiSet([
-    ['token', JSON.stringify(token)],
-    ['loggedIn', JSON.stringify('yes')],
+    ['token', token],
+    ['loggedIn', 'yes'],
   ]);
   isLoggedInVar(true);
   token(token);
