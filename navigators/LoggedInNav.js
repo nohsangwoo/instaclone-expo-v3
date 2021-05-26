b08caa61eec1a952e7954267f017aa005d9e0706
nohsangwoo <nsgr12@naver.com>
2021-05-26 22:13:54 +0900

    #15.0 Tab Navigator

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -12,9 +12,11 @@ export default function LoggedInNav() {
   return (
     <Tabs.Navigator
       tabBarOptions={{
+        // 클릭한 tab네비게이션이
         activeTintColor: 'white',
         showLabel: false,
         style: {
+          // tabNavigation의 상단부분의 선을 투명하게 만들어줌
           borderTopColor: 'rgba(255, 255, 255, 0.3)',
           backgroundColor: 'black',
         },
@@ -24,7 +26,10 @@ export default function LoggedInNav() {
         name="Feed"
         component={Feed}
         options={{
+          // 이 props의 color는 activeTintColor랑 같음
+          // focused는 현재 해당 route가 활성화된 상태인가 아닌가를 boolean으로 표시함
           tabBarIcon: ({ focused, color, size }) => (
+            // 해당 탭의 화면이 활성화된 상태라면 icon의 크기가 24로되고 평소엔 20의 크기
             <Ionicons name="home" color={color} size={focused ? 24 : 20} />
           ),
         }}
