5877ab1122ad3dbbe2120245b7211f0bc266a0f2
nohsangwoo <nsgr12@naver.com>
2021-05-27 15:12:51 +0900

    #15.5 FlatList

--- README.md
+++ README.md
@@ -187,3 +187,8 @@ https://github.com/localtunnel/localtunnel
 # 15.4 Apollo Auth
 
 - navigation header에 이미지 삽입
+- apollo setting
+
+# 15.5 FlatList
+
+화면에 나타나는 컴포넌트만 일단 렌더링하고 화면의 범위를 넘어서는 부분의 컴포넌트는 스크롤 하는경우에 추가로 렌더링 한다
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,6 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import ScreenLayout from '../components/ScreenLayout';
 import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from '../fragments';
 
 // seeFeed trigger생성
@@ -25,20 +27,31 @@ const FEED_QUERY = gql`
   ${COMMENT_FRAGMENT}
 `;
 
-export default function Feed({ navigation }) {
-  const { data } = useQuery(FEED_QUERY);
-  console.log(data);
+export default function Feed() {
+  const { data, loading } = useQuery(FEED_QUERY);
+  const renderPhoto = ({ item: photo }) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ color: 'white' }}>{photo.caption}</Text>
+      </View>
+    );
+  };
 
   return (
-    <View
-      style={{
-        backgroundColor: 'black',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text style={{ color: 'white' }}>Feed</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      {/* map이랑 비슷하다 someList.map((data,index)=>{
+        return (<RenderComponent>some contents</RenderComponent>);
+      }) */}
+      <FlatList
+        // 사용법
+        // 사용되는 데이터(리스트)를 넣어주고
+        data={data?.seeFeed}
+        // 고유 식별자 (map돌릴때 key랑 같은기능)
+        // key={photo.id} 라는 뜻이랑 같다
+        keyExtractor={photo => '' + photo.id}
+        // 해당 데이터를 어떻게 가공해서 렌더링할건지 정한 컴포넌트를 집어넣고
+        renderItem={renderPhoto}
+      />
+    </ScreenLayout>
   );
 }
