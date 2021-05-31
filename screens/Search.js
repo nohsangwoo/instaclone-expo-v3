ded4c8ad0bfd07eb81b86fbe51cb6f86334b3f9f
nohsangwoo <nsgr12@naver.com>
2021-05-31 11:44:47 +0900

    #16.5 Search part Three

--- screens/Search.js
+++ screens/Search.js
@@ -15,13 +15,31 @@ const SEARCH_PHOTOS = gql`
   }
 `;
 
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
-  const { setValue, register } = useForm();
+  const { setValue, register, watch, handleSubmit } = useForm();
   // 나중에 startQueryFn이 호출될때 쿼리문이 실행됨
   // startQueryFn라는 이름의 동작 트리거를 생성
-  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
 
   const SearchBox = () => (
     <TextInput
@@ -33,25 +51,39 @@ export default function Search({ navigation }) {
       returnKeyType="search"
       autoCorrect={false}
       onChangeText={text => setValue('keyword', text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register('keyword');
+    register('keyword', {
+      required: true,
+      minLength: 3,
+    });
   }, []);
+  console.log(data);
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: 'black',
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text style={{ color: 'white' }}>Photo</Text>
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size="large" />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined &&
+        data?.searchPhotos?.length === 0 ? (
+          <MessageContainer>
+            <MessageText>Could not find anything.</MessageText>
+          </MessageContainer>
+        ) : null}
       </View>
     </DismissKeyboard>
   );
